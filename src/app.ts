a9df4bcde3a67f4fba38b08185f2178935f94565
Hans-Peter Martini <hapemartini@gmail.com>
2021-01-21 07:09:35 +0100

    add runtime measurement

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { MergeIntervals } from "./merge-intervals";
+const start = new Date().getMilliseconds();
 
 const args = process.argv.slice(2);
 
@@ -11,4 +12,7 @@ try {
 } catch (error) {
   console.log("Error parsing arguments:", args);
   console.log(error);
+} finally {
+  const executionTime = new Date().getMilliseconds() - start;
+  console.log("Execution time was: %dms", executionTime);
 }
